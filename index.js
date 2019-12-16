84f2e3b6c4c232daba1b18d2d24ff2f154d67eee
suchith.sathyanarayana <suchith.sathyanarayana@bshg.com>
2019-12-16 14:27:29 +0530

    finished starting a server

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Yo");
+/*
+ * Primary file api
+ *
+ */ 
+
+ // Dependencies
+ var http = require("http");
+
+ // server should response to all the request with valid string
+var server = http.createServer(function(req,res){
+    res.end("Server created\n");
+})
+ // start the server and run on port 3000
+server.listen(3000, function(){
+    console.log("Server is running on port 3000");
+});
